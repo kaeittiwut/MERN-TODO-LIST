58cf0b449ab8a459a6c3dd38779aef73dbf5c669
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2020-11-10 00:12:09 +0700

    connecting to backend

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,26 +1,32 @@
-import uuid from 'react-uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING,
+} from '../actions/types';
 
 const initialState = {
-  items: [
-    { id: uuid(), name: 'Test1' },
-    { id: uuid(), name: 'Test2' },
-    { id: uuid(), name: 'Test3' },
-    { id: uuid(), name: 'Test4' },
-  ],
+  items: [],
+  loading: false,
 };
 
+// eslint-disable-next-line
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
-      return { ...state };
+      return { ...state, items: action.payload, loading: false };
     case DELETE_ITEM:
       return {
         ...state,
-        items: state.items.filter((item) => item.id !== action.payload),
+        items: state.items.filter((item) => item._id !== action.payload),
       };
     case ADD_ITEM:
       return { ...state, items: [action.payload, ...state.items] };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
     default:
       return state;
   }
