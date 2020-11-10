e53ae1286e5adbed7148cba68147f17c37117455
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2020-11-11 01:19:37 +0700

    add JWT auth

--- config/default.js
+++ config/default.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     'mongodb+srv://<user>:<password>@cluster0.bl9mw.mongodb.net/<dbname>?retryWrites=true&w=majority', //Example of your MongoDB Atlas URI
+  jwtSecret: 'sl_myJwtSecret',
 };
