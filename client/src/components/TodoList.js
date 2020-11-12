789640c1c86a40f7dd1d0bac9165d63155f047c0
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2020-11-12 15:22:56 +0700

    add Login and Access control on React

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -6,6 +6,12 @@ import { getItems, deleteItem } from '../actions/todoActions';
 import PropTypes from 'prop-types';
 
 class TodoList extends Component {
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool,
+  };
+
   componentDidMount() {
     this.props.getItems();
   }
@@ -23,14 +29,16 @@ class TodoList extends Component {
             {items.map(({ _id, name }) => (
               <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)}
-                  >
-                    X
-                  </Button>
+                  {this.props.isAuthenticated ? (
+                    <Button
+                      className="remove-btn"
+                      color="danger"
+                      size="sm"
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                      X
+                    </Button>
+                  ) : null}
                   {name + ' ' + _id}
                 </ListGroupItem>
               </CSSTransition>
@@ -42,13 +50,9 @@ class TodoList extends Component {
   }
 }
 
-TodoList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired,
-};
-
 const mapStateToProps = (state) => ({
   item: state.item,
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 export default connect(mapStateToProps, { getItems, deleteItem })(TodoList);
